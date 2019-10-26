960a159a60eb2e35d986b3c15d3b1f424acacd90
Yuhan Xie <42077053+ScorpionXiezi@users.noreply.github.com>
2019-10-26 16:47:28 -0500

    1

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
 import parselmouth
-
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import wave
+import sys
+import math
+import contextlib
 
 sns.set() # Use seaborn's default style to make attractive graphs
 
 # Plot nice figures using Python's "standard" matplotlib library
-snd = parselmouth.Sound("")
+snd = parselmouth.Sound("Demo.wav")
+
 plt.figure()
 plt.plot(snd.xs(), snd.values.T)
 plt.xlim([snd.xmin, snd.xmax])
 plt.xlabel("time [s]")
 plt.ylabel("amplitude")
-plt.show() # or plt.savefig("sound.png"), or plt.savefig("sound.pdf")
+#plt.show() # or plt.savefig("sound.png"), or plt.savefig("sound.pdf")
+
+def draw_spectrogram(spectrogram, dynamic_range=70):
+    X, Y = spectrogram.x_grid(), spectrogram.y_grid()
+    sg_db = 10 * np.log10(spectrogram.values)
+    plt.pcolormesh(X, Y, sg_db, vmin=sg_db.max() - dynamic_range, cmap='afmhot')
+    plt.ylim([spectrogram.ymin, spectrogram.ymax])
+    plt.xlabel("time [s]")
+    plt.ylabel("frequency [Hz]")
+
+def draw_intensity(intensity):
+    plt.plot(intensity.xs(), intensity.values.T, linewidth=3, color='w')
+    plt.plot(intensity.xs(), intensity.values.T, linewidth=1)
+    plt.grid(False)
+    plt.ylim(0)
+    plt.ylabel("intensity [dB]")
+
+intensity = snd.to_intensity()
+spectrogram = snd.to_spectrogram()
+print(spectrogram.values.size)
+
+print(spectrogram.values.size)
+
+plt.figure()
+
+draw_spectrogram(spectrogram)
+plt.twinx()
+draw_intensity(intensity)
+plt.xlim([snd.xmin, snd.xmax])
+#plt.show() # or plt.savefig("spectrogram.pdf")
